96eed7400d8f831ca3ca7fb22fc06178d7aeeb46
kenndai <kdai002@ucr.edu>
2021-03-01 10:35:01 -0800

    did 4 queries

--- Lab7/queries.sql
+++ Lab7/queries.sql
@@ -4,10 +4,23 @@ from part_nyc nyc
 where nyc.on_hand > 70;
 
 -- Count how many total parts on hand, in both NYC and SFO, are Red
+select sum(*)
+from	(select sum(nyc.on_hand), sum(sfo.on_hand)
+	from part_nyc nyc, part_sfo sfo
+	where nyc.color = 0 and sfo.color = 0);
 
 -- List all the suppliers that have more total on hand parts in NYC than they do in SFO
+select nyc.supplier 
+from part_nyc nyc
+where nyc.supplier IN 	(select nyc2.supplier
+			 from part_nyc nyc2, part_sfo sfo
+			 where nyc2.on_hand > sfo.on_hand);
 
 -- List all suppliers that supply parts in NYC that aren't supplied by anyone in SFO
+select nyc2.supplier
+from part_nyc nyc2 
+where nyc2.part_num NOT IN	(select sfo.part_num
+				 from part_sfo sfo);
 
 -- Update all of the NYC on hand values to on hand - 10
 
