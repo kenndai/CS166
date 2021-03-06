716955472e60faa360bd1f5423da7ce6c260c29d
kenndai <kdai002@ucr.edu>
2021-03-05 21:38:17 -0800

    fixed syntax errors with create_indexes

--- Lab7/create_indexes.sql
+++ Lab7/create_indexes.sql
@@ -1,9 +1,7 @@
 create index nyc_partnum_index
 on part_nyc
-[USING BTREE]
-(part_number)
+USING BTREE (part_number);
 
 create index sfo_partnum_index
 on part_sfo
-[USING BTREE]
-(part_number)
+USING BTREE (part_number);
