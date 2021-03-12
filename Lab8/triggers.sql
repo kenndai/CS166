ae37023dbe3313df7bfdf64493e05dc0a32940f3
kenndai <kdai002@ucr.edu>
2021-03-11 16:15:31 -0800

    finished lab 8

--- Lab8/triggers.sql
+++ Lab8/triggers.sql
@@ -1,21 +1,16 @@
-:CREATE SEQUENCE part_number_seq START WITH 50000;
---nextval(name), currval(name), setval(name)
+CREATE SEQUENCE part_number_seq START WITH 50000;
 
-
---your task is to implement a trigger and procedure to automatically populate part number with
---incremented value upon insertion of the new row into part nyc. After that your insert statements
---should not include value for part number.
-
--- Create a procedure that will return next value of the aforementioned sequence. Use function
--- nextval(part_number_seq) to get the next value from the sequence.
 CREATE LANGUAGE plpgsql;
-CREATE OR REPLACE FUNCTION auto_part_number() RETURNS "trigger" AS $insert_record$
+CREATE OR REPLACE FUNCTION auto_part_number()
+RETURNS "trigger" AS
+$BODY$
     BEGIN
-        RETURN nextval(part_number_seq);
+	NEW.part_number := nextval('part_number_seq');
+	RETURN NEW;
     END;
+$BODY$
 LANGUAGE plpgsql VOLATILE;
 
---create a trigger calling the procedure upon insertion of a new record
-CREATE TRIGGER insert_record
-AFTER INSERT ON part_nyc
-    FOR EACH STATEMENT EXECUTE PROCEDURE auto_part_num();
+CREATE TRIGGER insert_record BEFORE INSERT 
+ON part_nyc FOR EACH ROW
+EXECUTE PROCEDURE auto_part_number();
