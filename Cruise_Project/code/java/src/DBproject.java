b8fd1c293c97964482988fcee82cefb31e89418b
kenndai <heyitskennydai@gmail.com>
2021-03-20 21:47:20 -0700

    more testing available seats query

--- Cruise_Project/code/java/src/DBproject.java
+++ Cruise_Project/code/java/src/DBproject.java
@@ -452,12 +452,14 @@ public class DBproject{
 			System.out.print("Please enter the cruise's departure date: ");
 			String departure_date = in.readLine();
 
-			String query = String.format("select count(s.seats) - count(r.status) as Available_Seats from CruiseInfo c, Ship s, Reservation r where c.cruise_id = %d AND c.ship_id = s.id AND r.cid = %d AND r.status = 'C' OR r.status = 'R'", cnum, cnum);
-	//		String query = String.format("select count(s.seats) - taken.seatsTaken from CruiseInfo c, Ship s, (select count(*) from Reservation r where r.status = 'C' OR r.status = 'R' AND r.cid = %d) as taken where c.cruise_id = %d AND c.ship_id = s.id", cnum, cnum);
-			System.out.println("TESTTTING");
+			String query = String.format("select count(*) from Reservation r where r.status = 'C' OR r.status = 'R' AND r.cid = %d", cnum);
 			int rowCount = esql.executeQueryAndPrintResult(query);
-
 			System.out.println ("total row(s): " + rowCount);
+
+			String query = String.format("select count(s.seats) from CruiseInfo c, Ship s where c.cruise_id = %d AND c.ship_id = s.id", cnum);
+			int rowCount = esql.executeQueryAndPrintResult(query);
+			System.out.println ("total row(s): " + rowCount);
+
 
 		} catch(Exception e) {
 			System.err.println(e.getMessage());
